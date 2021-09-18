0fcddc01bbf553b9147587d3d0349abfbef33bb7
Ayoub <esmailzadeh.ayub@yahoo.com>
2021-09-18 13:38:57 +0430

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,42 @@
+resource "aws_lb" "app" {
+  name               = "main-app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = $aws_subnet.pub_subnet.id
+  security_groups    = $aws_security_group" "ec2_sg
+}
+
+resource "aws_lb_listener" "app" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.my_taget_group.arn
+  }
+}
+ resource "aws_lb_target_group" "my-target-group" {
+  health_check {
+    interval            = 10
+    path                = "/"
+    protocol            = "HTTP"
+    timeout             = 5
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+  }
+
+  name        = "my-test-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = "${var.vpc_id}"
+}
+resource "aws_lb_target_group_attachment" "my-alb-target-group-attachment1" {
+  target_group_arn = "${aws_lb_target_group.my-target-group.arn}"
+  target_id        = "${aws_instance.ec2_instance.id}"
+  port             = 80
+}
 resource "aws_key_pair" "deployer" {     # Creating the Key pair on AWS 
   key_name   = "deployer-key"
   public_key = "${file("~/.ssh/id_rsa.pub")}" # Generated private and public key on local machine
